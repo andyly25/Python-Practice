a3b9d2e26b777d014cf0d68d080cf4bde836c52c
Andy Ly <andywindsong@gmail.com>
2017-05-16 17:01:26 -0700

    refresher exercise

--- web_application_project/exercise/pizzerias/urls.py
+++ web_application_project/exercise/pizzerias/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # home page
     url(r'^$', views.index, name='index'),
 
-
+    # Show all pizzas
+    url(r'^pizzas/$', views.pizzas, name='pizzas'),
+    # Detailed for single pizza
+    url(r'^pizzas/(?P<pizza_id>\d+)/$', views.pizza, name='pizza')
 ]
--- web_application_project/exercise/pizzerias/views.py
+++ web_application_project/exercise/pizzerias/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def index(request):
     return render(request, 'pizzerias/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzerias/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('-date_added')
+    context = {'pizza':pizza, 'toppings':toppings}
+    return render(request, 'pizzerias/pizza.html', context)
