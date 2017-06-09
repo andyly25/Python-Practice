2d0a690c651d07b0eac124f87b8027af3ab5dfe3
Andy Ly <andywindsong@gmail.com>
2017-06-09 12:21:44 -0700

    playing with dice

--- data visualization/e04_die_visual.py
+++ data visualization/e04_die_visual.py
@@ -1,30 +1,36 @@
 import pygal
 from e04_die import Die
 
-die = Die()
+die1 = Die()
+die2 = Die()
 
 # store rolls into a list
 results = []
 # Using Pygal allows an increase in simulated rolls
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(10000):
+    result = die1.roll() + die2.roll()
     results.append(result)
 
 # ANalyze frequencies by counting how many time each num appears
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # print(frequencies)
 
 # visualize the results
+# create a bar chart by creating instance of pygal.Bar()
 hist = pygal.Bar()
 
-hist.title = "results of rolling one 6 sided Dice 1k times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+# make the labels
+hist.title = "results of rolling two 6 sided Dice 10000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequency of result"
 
-hist.add("D6", frequencies)
-hist.render_to_file('e04_die_visual.svg')
+# add series of values to the chart
+hist.add("D6 + D6", frequencies)
+# render chart into svg file, easiest way to view is in web browser
+hist.render_to_file('e04_die_visual2.svg')
